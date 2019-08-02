34542200336d655067880652b70fd7023bdf9840
trance-cyberian <trance.cyberian@gmail.com>
2019-08-02 14:34:43 +0200

    first half of the day finished

--- bookstore.js
+++ bookstore.js
@@ -12,61 +12,71 @@ function accessingTheOnLineJSON() {
 function fetchingDataFromOnLineJSON(url) {
     fetch(url, {
             method: "GET",
-            /*headers: {
-                "X-API-Key": "ZnV9hZHe5Q6tOdwr8zo6XrMJfTcCGLLqxSe1VzhO"
-            }*/
         })
+
         .then(response => {
             console.log(response);
             return response.json();
         })
+
         .then(function (data) {
-            
-            //Checking the information from the object
+            //1.Checking the accessibility to the array            
+            //1-a.Checking the information from the object
             console.log("Console data" + " ", data)
             console.log("Console data.books" + " ", data.books)
-            
-            //Defining my object to work with
+
+            //1-b.Defining my object to work with
             let books = data.books
             console.log("data.books = books" + " ", books);
-            
-            //Getting the keys for the element [0]
+
+            //1-c.Getting the keys for the element [0]
             Object.keys(books[0]).forEach(item => {
-                console.log("console keys" + " ", item);
+                console.log("Console object's keys" + " ", item);
             });
 
-            
-            
+            //2.Creating table dynamically
 
+            function getCoverImageFromLink(books) {
+                let booksL = books.length
+                var i
+                for (i = 0; i < booksL; i++) {
+                    let coverImg = books[i].cover;
+                    return coverImg;
+                }
+                console.log(coverImg)
+            }
 
+            var img = document.createElement("IMG");
+            img.src = getCoverImageFromLink(books);
+            bookshelf.appendChild(img);
 
+            function buildBookshelf(books, bookshelf) {
+                var tbody = document.getElementById(bookshelf)
+                let booksL = books.length
+                var i
+                for (i = 0; i < booksL; i++) {
+                    var row = document.createElement("TR")
 
+                    var td = document.createElement("TD")
+                    td.innerHTML = getCoverImageFromLink(books)
+                    row.appendChild(td)
 
+                    tbody.appendChild(row)
+                }
+            }
+            buildBookshelf(books, bookshelf);
 
-
-
-            /*console.log(data.results[0].members);
-            let members = data.results[0].members;
-            let statistics = dataStat.statistics;
-
-            mapMembersParty(members);
-            mapMembersVotes(members);
-
-            buildTable(statistics);
-            
-            if (document.title.includes("Loyalty")){
-                sortAscending(members, "votes_with_party_pct");
-                feedsLeastTable(members);
-
-                sortDescending(members, "votes_with_party_pct");
-                feedsMostTable(members);
-            }else{
-                sortAscending(members, "missed_votes_pct");
-                feedsHighestAttendanceTable(members);
-
-                sortDescending(members, "missed_votes_pct");
-                feedsLowestAttendanceTable(members);
-            }*/
+            //2.Assigning content to the grid
+            //2-a.Bookcovers
+            /*
+                        function insertBookcoverImg (id) {
+                            let coverImg = document.getElementById("book id").innerHTML
+                            coverImg.src = books[0].cover
+                            book.appendChild(coverImg);
+                            insertBookcoverImg (r1c1);
+                        }
+                            console.log(coverImg)
+            */
         })
 
         .catch(function (error) {
